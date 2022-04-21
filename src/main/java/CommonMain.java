9659a05294aa9cee6c692f3465b40d720c96895a
daniilchukhin <daniil.chukhin@firedome.io>
2022-04-21 18:09:41 +0300

    Test

--- src/main/java/CommonMain.java
+++ src/main/java/CommonMain.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class CommonMain {
+public class CommonMain {
+
+    public static void main(String[] args) {
+        System.out.println(CommonMain.class.getPackage().getImplementationVersion());
+    }
 }
